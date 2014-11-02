934df2c48b8404e854f2c4c6170dba7cd3a8a079
Egor <sapronov.egor@gmail.com>
2014-11-02 15:04:13 +0600

    Rename

--- libs/models/imageSchema.js
+++ libs/models/imageSchema.js
@@ -4,16 +4,16 @@ var Image = new Schema({
     geoLoc: [],
     description: String,
     uploaded: Date,
-    likes: [
+    userLiked: [
         {type: Schema.ObjectId, ref: 'UserModel'}
     ],
     user: {type: Schema.ObjectId, ref: 'UserModel'},
     image: Buffer
 });
 
-Image.virtual('likesCount')
+Image.virtual('likes')
     .get(function () {
-        this.populate('likes')
+        this.populate('userLiked')
             .exec(function (err, image) {
                 return image.likes.length;
             });
