25993c615da273750c3eab53239cb0862f0a2f38
Egor <sapronov.egor@gmail.com>
2014-11-02 15:20:31 +0600

    Models from image and comment schemas

--- libs/mongo.js
+++ libs/mongo.js
@@ -6,6 +6,9 @@ var User = require('./models/authSchema').User,
     Client = require('./models/authSchema').Client,
     AccessToken = require('./models/authSchema').AccessToken;
 
+var Image = require('./models/imageSchema').Image,
+    Comment = require('./models/commentSchema').Comment;
+
 mongoose.connect(config.get('mongoose:uri'));
 var db = mongoose.connection;
 
@@ -21,7 +24,14 @@ var UserModel = mongoose.model('User', User),
     ClientModel = mongoose.model('Client', Client),
     AccessTokenModel = mongoose.model('AccessToken', AccessToken);
 
+// Content models
+var ImageModel = mongoose.model('Image', Image),
+    CommentModel = mongoose.model('Comment', Comment);
+
 module.exports.UserModel = UserModel;
 module.exports.ClientModel = ClientModel;
 module.exports.AccessTokenModel = AccessTokenModel;
+
+module.exports.ImageModel = ImageModel;
+module.exports.CommentModel = CommentModel;
 
