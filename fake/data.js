6c038278438670514d401715d4a8f462a9053cc9
Egor <sapronov.egor@gmail.com>
2014-11-16 20:31:48 +0600

    SMall fix

--- fake/data.js
+++ fake/data.js
@@ -1,23 +1,23 @@
-var log                 = require('../libs/log')(module);
-var mongoose            = require('../libs/mongo').mongoose;
-var UserModel           = require('../libs/mongo').UserModel;
-var ClientModel         = require('../libs/mongo').ClientModel;
-var AccessTokenModel    = require('../libs/mongo').AccessTokenModel;
+var log = require('../libs/log')(module);
+var mongoose = require('mongoose');
+var UserModel = require('../libs/mongo').UserModel;
+var ClientModel = require('../libs/mongo').ClientModel;
+var AccessTokenModel = require('../libs/mongo').AccessTokenModel;
 
-UserModel.remove({}, function(err) {
-    var user = new UserModel({ username: "egor", password: "123456" });
-    user.save(function(err, user) {
-        if(err) return log.error(err);
-        else log.info("New user - %s:%s",user.username,user.password);
+UserModel.remove({}, function (err) {
+    var user = new UserModel({username: "egor", password: "123456"});
+    user.save(function (err, user) {
+        if (err) return log.error(err);
+        else log.info("New user - %s:%s", user.username, user.password);
     });
 
 });
 
-ClientModel.remove({}, function(err) {
-    var client = new ClientModel({ name: "OurService iOS client v1", clientId: "web", clientSecret:"qwerty" });
-    client.save(function(err, client) {
-        if(err) return log.error(err);
-        else log.info("New client - %s:%s",client.clientId,client.clientSecret);
+ClientModel.remove({}, function (err) {
+    var client = new ClientModel({name: "OurService iOS client v1", clientId: "web", clientSecret: "qwerty"});
+    client.save(function (err, client) {
+        if (err) return log.error(err);
+        else log.info("New client - %s:%s", client.clientId, client.clientSecret);
     });
 });
 AccessTokenModel.remove({}, function (err) {
