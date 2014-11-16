6e7feeef426513ad9ab9b3523130855708806edb
Egor <sapronov.egor@gmail.com>
2014-11-16 20:49:47 +0600

    Create new user.

--- libs/oauth2.js
+++ libs/oauth2.js
@@ -4,13 +4,14 @@ var crypto = require('crypto');
 var UserModel = require('./mongo').UserModel;
 var ClientModel = require('./mongo').ClientModel;
 var AccessTokenModel = require('./mongo').AccessTokenModel;
+var log = require('./log')(module);
 
 // create OAuth 2.0 server
 var server = oauth2orize.createServer();
 
 // Exchange username & password for access token.
 server.exchange(oauth2orize.exchange.password(function (client, username, password, scope, done) {
-    UserModel.findOne({ username: username }, function (err, user) {
+    UserModel.findOne({username: username}, function (err, user) {
         if (err) {
             return done(err);
         }
@@ -21,15 +22,15 @@ server.exchange(oauth2orize.exchange.password(function (client, username, passwo
             return done(null, false);
         }
 
-        AccessTokenModel.remove({ userId: user.userId, clientId: client.clientId }, function (err) {
+        AccessTokenModel.remove({userId: user.userId, clientId: client.clientId}, function (err) {
             if (err) return done(err);
         });
 
         var tokenValue = crypto.randomBytes(32).toString('base64');
 
-        var token = new AccessTokenModel({ token: tokenValue, clientId: client.clientId, userId: user.userId });
+        var token = new AccessTokenModel({token: tokenValue, clientId: client.clientId, userId: user.userId});
 
-        var info = { scope: '*' }
+        var info = {scope: '*'}
         token.save(function (err, token) {
             if (err) {
                 return done(err);
@@ -39,10 +40,23 @@ server.exchange(oauth2orize.exchange.password(function (client, username, passwo
     });
 }));
 
+function register(username, password) {
+    var user = new UserModel({username: username, password: password});
+
+    user.save(function (err, user) {
+        if (err) {
+            return log.error(err);
+        }
+        else {
+            log.info("New user - %s:%s", user.username, user.password);
+        }
+    });
+}
+
 
 // token endpoint
 exports.token = [
-    passport.authenticate(['basic', 'oauth2-client-password'], { session: false }),
+    passport.authenticate(['basic', 'oauth2-client-password'], {session: false}),
     server.token(),
     server.errorHandler()
 ];
