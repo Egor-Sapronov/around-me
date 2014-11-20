c5fc69c65f265ad66515b974ffa8979546acea9f
Egor <sapronov.egor@gmail.com>
2014-11-20 22:16:23 +0600

    Add map on page

--- client/app/init.js
+++ client/app/init.js
@@ -15,5 +15,15 @@ require('./login/login.js');
         })
         .config(function ($httpProvider) {
             $httpProvider.interceptors.push('TokenInterceptor');
-        })
+        });
 })();
+
+function initialize() {
+    var mapOptions = {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+        mapOptions);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
