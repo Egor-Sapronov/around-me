9c7840b9d1aed00a83a95784eea0708e6f26c1bc
Egor <sapronov.egor@gmail.com>
2014-12-05 11:31:11 +0600

    Now start mongo

--- app.js
+++ app.js
@@ -5,16 +5,12 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 
-require('./libs/auth');
-
 app.set('views', path.join(__dirname, 'client/build/templates'));
 app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-//app.use(passport.initialize());
-
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/',
