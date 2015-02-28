30352e612c01023fd4ddceb3c44fa0bf21e46b41
Egor <sapronov.egor@gmail.com>
2015-02-28 22:01:41 +0600

    fix

--- client/src/app/components/profileCard.jsx
+++ client/src/app/components/profileCard.jsx
@@ -3,17 +3,13 @@
 var ProfileCard = React.createClass({
     render: function () {
         return <div className="card">
-            <div className="card-image">
-                <img src={this.props.user.image} className="responsive-img" />
-            </div>
-            <div className="card-content">
-                <a className="collection-item waves-effect waves-light-green-lighten-1">{this.props.user.name}
-                    <span className="badge">share
-                        <i className="mdi-social-share right"></i>
-                    </span>
-                </a>
-            </div>
-        </div>;
+                <div className="card-image">
+                    <img src={this.props.user.image} className="responsive-img" />
+                </div>
+                <div className="card-content">
+                    <h2>{this.props.user.name}</h2>
+                </div>
+            </div>;
     }
 });
 
