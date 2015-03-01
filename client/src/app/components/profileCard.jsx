1690e46ffdcf989fbd9728bae1d5c3d83c9f3df1
Egor <sapronov.egor@gmail.com>
2015-03-01 17:28:43 +0600

    fix

--- client/src/app/components/profileCard.jsx
+++ client/src/app/components/profileCard.jsx
@@ -3,22 +3,22 @@
 var ProfileCard = React.createClass({
     render: function () {
         return <div>
-            <div className="row">
-                <div className="col l12 valign-wrapper">
-                    <img src={this.props.user.image} className="valign center-align circle responsive-img" />
-                    <p className="flow-text valign center-align">{this.props.user.name}</p>
+            <div className="row valign-wrapper">
+                <div className="col l2 s12 valign center-align">
+                    <img src={this.props.user.image} className="circle responsive-img" />
+                    <p className="flow-text">{this.props.user.name}</p>
                 </div>
             </div>
             <div className="container">
                 <div className="row">
                     <div className="col l4 s12 valign-wrapper">
-                        <p className="flow-text valign">followers</p>
+                        <p className="flow-text center-align">followers</p>
                     </div>
                     <div className="col l4 s12 valign-wrapper">
-                        <p className="flow-text valign">photos</p>
+                        <p className="flow-text center-align">photos</p>
                     </div>
                     <div className="col l4 s12 valign-wrapper">
-                        <p className="flow-text valign">likes</p>
+                        <p className="flow-text center-align">likes</p>
                     </div>
                 </div>
             </div>
