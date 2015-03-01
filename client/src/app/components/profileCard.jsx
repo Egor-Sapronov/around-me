0f450524dc7bb362686324b44ed32df6efd354dd
Egor <sapronov.egor@gmail.com>
2015-03-01 17:14:01 +0600

    style(profile)

--- client/src/app/components/profileCard.jsx
+++ client/src/app/components/profileCard.jsx
@@ -2,12 +2,27 @@
 
 var ProfileCard = React.createClass({
     render: function () {
-        return <div className="row valign-wrapper">
-            <div className="col l2 s12 center-align">
-                <img src="{this.props.user.image}" className="circle responsive-img" />
-                <p className="flow-text">{this.props.user.name}</p>
+        return <div>
+            <div className="row valign-wrapper">
+                <div className="col l2 s12 center-align valign">
+                    <img src="{this.props.user.image}" className="circle responsive-img" />
+                    <p className="flow-text">{this.props.user.name}</p>
+                </div>
             </div>
-        </div>;
+            <div className="container">
+                <div className="row">
+                    <div className="col l4 s12 valign-wrapper">
+                        <p className="flow-text valign">followers</p>
+                    </div>
+                    <div className="col l4 s12 valign-wrapper">
+                        <p className="flow-text valign">photos</p>
+                    </div>
+                    <div className="col l4 s12 valign-wrapper">
+                        <p className="flow-text valign">likes</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     }
 });
 
