15481432ef0394e91bbffec4e298a4c0fcb3c598
Egor <sapronov.egor@gmail.com>
2015-03-01 21:00:31 +0600

    style(profile)

--- client/src/app/components/profileCard.jsx
+++ client/src/app/components/profileCard.jsx
@@ -2,27 +2,30 @@
 
 var ProfileCard = React.createClass({
     render: function () {
-        return <div>
-            <div className="row valign-wrapper">
-                <div className="col l2 s12 valign">
-                    <img src={this.props.user.image} className="circle responsive-img" />
-                    <p className="flow-text center-align">{this.props.user.name}</p>
-                </div>
-            </div>
-            <div className="container">
+        return < div >
+            <main className="container">
                 <div className="row">
-                    <div className="col l4 s12 valign-wrapper">
-                        <p className="flow-text center-align">followers</p>
-                    </div>
-                    <div className="col l4 s12 valign-wrapper">
-                        <p className="flow-text center-align">photos</p>
-                    </div>
-                    <div className="col l4 s12 valign-wrapper">
-                        <p className="flow-text center-align">likes</p>
+                    <div className="col l3 offset-l4 s12">
+                        <img src={this.props.user.image} className="circle responsive-img center-align" />
+                        <p className="center-align flow-text">{this.props.user.name}</p>
                     </div>
                 </div>
-            </div>
-        </div>
+            </main>
+            <div className="divider"></div>
+            <aside className="container">
+                <div className="row">
+                    <div className="col l3 offset-l1 s12">
+                        <p className="center-align flow-text">Followers</p>
+                    </div>
+                    <div className="col l3 s12">
+                        <p className="center-align flow-text">Photos</p>
+                    </div>
+                    <div className="col l3 s12">
+                        <p className="center-align flow-text">Likes</p>
+                    </div>
+                </div>
+            </aside>
+        </div>;
     }
 });
 
