ca878608f9d00655f07561d0dd5d7e53702ef36d
Egor <sapronov.egor@gmail.com>
2015-03-01 17:02:38 +0600

    fix(profile card): class -> className

--- client/src/app/components/profileCard.jsx
+++ client/src/app/components/profileCard.jsx
@@ -2,10 +2,10 @@
 
 var ProfileCard = React.createClass({
     render: function () {
-        return <div class="row valign-wrapper">
-            <div class="col l2 s12 center-align">
-                <img src="{this.props.user.image}" class="circle responsive-img" />
-                <p class="flow-text">{this.props.user.name}</p>
+        return <div className="row valign-wrapper">
+            <div className="col l2 s12 center-align">
+                <img src="{this.props.user.image}" className="circle responsive-img" />
+                <p className="flow-text">{this.props.user.name}</p>
             </div>
         </div>;
     }
